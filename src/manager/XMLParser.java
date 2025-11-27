de76754b1465f73e2ab15ca7d7b8984360571205
Temi Bankole <temibankole5@gmail.com>
2025-11-27 10:55:01 -0700

    adding necessary imports and fields

--- src/manager/XMLParser.java
+++ src/manager/XMLParser.java
@@ -1,6 +1,34 @@
 package manager;
 
+//import java.io.BufferReader;
+//import java.io.FileReader;
+//import java.io.IOException;
+//
+//import implementations.MyStack;
+//import implementations.MyQueue;
+
+/**
+ * Responsible for parsing an XML file and reporting syntax errors 
+ * based on matching start and end tags. It will use the customized
+ * MyStack and MyQueue classes created for this assignment. 
+ * 
+ * @version 1.0
+ * @author Temi Bankole
+ */
+
 public class XMLParser
 {
+	/** Name of the XML file being parsed*/
+	private String filename;
+	
+	/**Stack to hold opening tags during parsing*/
+	
+	/**Queue to store error tags in the order they occur*/
+	
+	/**Queue to store extra unmatched closing tags */
+	
+	
+	
+	
 
 }
